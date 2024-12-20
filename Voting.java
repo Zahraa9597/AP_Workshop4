4fdf271a3dfd878f6515f64b52eb918055e2b634
zahraa <Zahraa9597>
2024-12-20 09:04:36 -0800

    add fields to Voting

--- Voting.java
+++ Voting.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Voting {
-    
+    private int type;//تک رأی و چند رأی ( 0 به معنای تک رأی و 1 به معنای چندرأی)
+    private String question; // پرسش رأیگیری
+    private HashMap<String, HashSet<Vote>> choices;
+    private boolean isAnonymous;
+    private ArrayList<Person> voters;
 }
