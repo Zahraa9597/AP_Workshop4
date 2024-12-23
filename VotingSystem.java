12013db762562b1b708c0b49163d24b9c883bc1d
zahraa <Zahraa9597>
2024-12-23 11:40:38 -0800

    added hashcode method to VotingSystem

--- VotingSystem.java
+++ VotingSystem.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class VotingSystem {
     private ArrayList<Voting> votingList;
@@ -13,5 +14,29 @@ public class VotingSystem {
     public ArrayList<Voting> getVotingList() {
         return votingList;
     }
-    
+    public void createVoting(String question, boolean isAnonymous, int type, ArrayList<String> choices) {
+
+    }
+    public void printResults(int index) {
+    }
+
+    public void printVoting(int index) {
+        Voting voting = getVoting(index);
+        System.out.println("Voting Question: " + voting.getQuestion());
+        System.out.println("Voting Type: " + voting.getType());
+        System.out.println("Choices:");
+        for (String choice : voting.getChoices().keySet()) {
+            System.out.println("- " + choice);
+        }
+    }
+
+    public void printVoters(int index) {
+        Voting voting = getVoting(index);
+        System.out.println("Voters for Voting Question: " + voting.getQuestion());
+        for (Person voter : voting.getVoters()) {
+            System.out.println("Voter: " + voter);
+        }
+    }
+
 }
+
